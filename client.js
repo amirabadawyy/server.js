9ce8c34d1133f8b946e311ddb6c7c10efadb70ce
Amira Badawy <amiraabouelezz2001@gmail.com>
2024-01-04 13:35:12 +0200

    Add files via upload

---
+++ client.js
@@ -0,0 +1,30 @@
+var socket=io.connect('https://myrep-g4m6.onrender.com')
+var username=document.getElementById('username')
+var message=document.getElementById('mess')
+var send=document.getElementById('send')
+var chat=document.getElementById('chat')
+var broadcast=document.getElementById('broadcast')
+send.addEventListener('click',function(){
+    socket.emit('message',{
+        username:username.value,
+        message:message.value
+    })
+
+});
+message.addEventListener('keypress',function(){
+socket.emit('broadcast',{
+    username:username.value,
+})
+
+})
+
+socket.on('new',function(info){
+    broadcast.innerHTML="";
+    chat.innerHTML+='<div class="container"><strong>'+info.username+':</strong>'
+   + info.message+
+   '</div>';
+});
+socket.on('new-broad',function(info){
+    broadcast.innerHTML ='<strong>'+info.username+': </strong> write message &#9993; &#9993; &#9993;';
+
+});
